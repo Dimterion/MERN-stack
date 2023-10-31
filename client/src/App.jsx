e51c55e6fa00fb9e5848223104955a2bb03b3600
Dmitrii <dimterion59@gmail.com>
2023-10-31 22:24:04 +0100

    upd header & make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function PrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+}
