e11137226eb5ffba3a1e73efaec5982d9333315e
Dmitrii <dimterion59@gmail.com>
2023-11-09 11:02:11 +0100

    add change / submit func to search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,8 +1,122 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Search() {
+  const navigate = useNavigate();
+
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: "",
+    type: "all",
+    certificate: false,
+    community: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+
+  console.log(listings);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const certificateFromUrl = urlParams.get("certificate");
+    const communityFromUrl = urlParams.get("community");
+    const offerFromUrl = urlParams.get("offer");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
+
+    if (
+      searchTermFromUrl ||
+      typeFromUrl ||
+      certificateFromUrl ||
+      communityFromUrl ||
+      offerFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
+    ) {
+      setSidebarData({
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        certificate: certificateFromUrl === "true" ? true : false,
+        community: communityFromUrl === "true" ? true : false,
+        offer: offerFromUrl === "true" ? true : false,
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
+      });
+    }
+
+    const fetchListings = async () => {
+      setLoading(true);
+
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      setListings(data);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "subscription" ||
+      e.target.id === "oneTimePurchase"
+    ) {
+      setSidebarData({ ...sidebarData, type: e.target.id });
+    }
+
+    if (e.target.id === "searchTerm") {
+      setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+    }
+
+    if (
+      e.target.id === "certificate" ||
+      e.target.id === "community" ||
+      e.target.id === "offer"
+    ) {
+      setSidebarData({
+        ...sidebarData,
+        [e.target.id]:
+          e.target.checked || e.target.checked === "true" ? true : false,
+      });
+    }
+
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+      const order = e.target.value.split("_")[1] || "desc";
+
+      setSidebarData({ ...sidebarData, sort, order });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const urlParams = new URLSearchParams();
+
+    urlParams.set("searchTerm", sidebarData.searchTerm);
+    urlParams.set("type", sidebarData.type);
+    urlParams.set("certificate", sidebarData.certificate);
+    urlParams.set("community", sidebarData.community);
+    urlParams.set("offer", sidebarData.offer);
+    urlParams.set("sort", sidebarData.sort);
+    urlParams.set("order", sidebarData.order);
+
+    const searchQuery = urlParams.toString();
+
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen md:max-w-md">
-        <form className="flex flex-col gap-8">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
           <div className="flex items-center gap-2">
             <label className="whitespace-nowrap font-semibold">
               Search term:
@@ -12,45 +126,88 @@ export default function Search() {
               id="searchTerm"
               placeholder="Search..."
               className="border rounded-lg p-3 w-full"
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
             />
           </div>
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="all" className="w-5" />
+              <input
+                type="checkbox"
+                id="all"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.type === "all"}
+              />
               <span>One-time purchase & subscription</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="subscription" className="w-5" />
+              <input
+                type="checkbox"
+                id="subscription"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.type === "subscription"}
+              />
               <span>Subscription</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="oneTimePurchase" className="w-5" />
+              <input
+                type="checkbox"
+                id="oneTimePurchase"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.type === "oneTimePurchase"}
+              />
               <span>One-time purchase</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.offer}
+              />
               <span>Offer</span>
             </div>
           </div>
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Details:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="certificate" className="w-5" />
+              <input
+                type="checkbox"
+                id="certificate"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.certificate}
+              />
               <span>Certificate</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="community" className="w-5" />
+              <input
+                type="checkbox"
+                id="community"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.community}
+              />
               <span>Community</span>
             </div>
           </div>
           <div className="flex items-center gap-2">
             <label className="font-semibold">Sort by:</label>
-            <select id="sort_order" className="border rounded-lg p-3">
-              <option>Price - decreasing</option>
-              <option>Price - increasing</option>
-              <option>Latest</option>
-              <option>Oldest</option>
+            <select
+              onChange={handleChange}
+              defaultValue={"created_at_desc"}
+              id="sort_order"
+              className="border rounded-lg p-3"
+            >
+              <option value="regularPrice_desc">Price - decreasing</option>
+              <option value="regularPrice_asc">Price - increasing</option>
+              <option value="createdAt_desc">Latest</option>
+              <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
           <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
