7479f655adeec83f12514afd6806c8146aeb1f80
Dmitrii <dimterion59@gmail.com>
2023-11-11 11:05:28 +0100

    add listing item comp & show listings

---
+++ client/src/components/ListingItem.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import { FaLink } from "react-icons/fa";
+
+export default function ListingItem({ listing }) {
+  return (
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          src={
+            listing.imageUrls[0] ||
+            "https://raw.githubusercontent.com/Dimterion/Portfolio-site/master/src/assets/image_panoramic.jpg"
+          }
+          alt="Listing cover."
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+        />
+        <div className="p-3 flex flex-col gap-2 w-full">
+          <p className="truncate text-lg font-semibold text-slate-700">
+            {listing.name}
+          </p>
+          <div className="flex items-center gap-1">
+            <FaLink className="h-4 w-4 text-green-700" />
+            <p className="text-sm text-gray-600 truncate w-full">
+              {listing.link}
+            </p>
+          </div>
+          <p className="text-sm text-slate-600 line-clamp-2">
+            {listing.description}
+          </p>
+          <p className="text-slate-500 mt-2 font-semibold">
+            {listing.offer
+              ? listing.discountPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
+            ${listing.type === "subscription" && " / month"}
+          </p>
+          <div className="text-slate-700 flex gap-4">
+            <div className="font-bold text-xs">
+              {listing.hours > 1
+                ? `${listing.hours} hours`
+                : `${listing.hours} hour`}
+            </div>
+            <div className="font-bold text-xs">
+              {listing.parts > 1
+                ? `${listing.parts} parts`
+                : `${listing.parts} part`}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
