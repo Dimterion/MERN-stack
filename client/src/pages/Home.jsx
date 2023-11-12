d762ad46eaa1c75d52c9725b71ca952de34a02e0
Dmitrii <dimterion59@gmail.com>
2023-11-12 10:11:20 +0100

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,162 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import ListingItem from "../components/ListingItem";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [offerListings, setOfferListings] = useState([]);
+  const [oneTimePurchaseListings, setOneTimePurchaseListings] = useState([]);
+  const [subscriptionListings, setSubscriptionListings] = useState([]);
+
+  SwiperCore.use([Navigation]);
+
+  console.log(offerListings);
+
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?offer=true&limit=4");
+        const data = await res.json();
+
+        setOfferListings(data);
+        fetchSubscriptionListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSubscriptionListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?type=subscription&limit=4");
+        const data = await res.json();
+
+        setSubscriptionListings(data);
+        fetchOneTimePurchaseListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchOneTimePurchaseListings = async () => {
+      try {
+        const res = await fetch(
+          "/api/listing/get?type=oneTimePurchase&limit=4"
+        );
+        const data = await res.json();
+
+        setOneTimePurchaseListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchOfferListings();
+  }, []);
+
+  return (
+    <div>
+      {/* Top */}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">coding</span>
+          <br />
+          resource.
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          MERN Stack.
+          <br />
+          Choose from various sources.
+        </div>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+        >
+          Start
+        </Link>
+      </div>
+      {/* Swiper */}
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[500px]"
+                key={listing._id}
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+      {/* Listings results */}
+      <div className="max-w-6xl mx-auto p-3 flex-col gap-8 my-10">
+        {offerListings && offerListings.length > 0 && (
+          <div>
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent offers
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={"/search?offer=true"}
+              >
+                Show more offers
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {offerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {subscriptionListings && subscriptionListings.length > 0 && (
+          <div>
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent subscriptions
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={"/search?type=subscription"}
+              >
+                Show more subscriptions
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {subscriptionListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {oneTimePurchaseListings && oneTimePurchaseListings.length > 0 && (
+          <div>
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent one-time purchases
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={"/search?type=oneTimePurchase"}
+              >
+                Show more one-time purchases
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {oneTimePurchaseListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
