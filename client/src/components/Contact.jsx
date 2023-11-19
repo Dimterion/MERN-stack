14136b23202df8ec8ef6eafd297eb28feaeb443f
Dmitrii <dimterion59@gmail.com>
2023-11-19 10:16:23 +0100

    adjust colors

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -45,7 +45,7 @@ export default function Contact({ listing }) {
           ></textarea>
           <Link
             to={`mailto:${poster.email}?subject=Regarding ${listing.name}&body=${message}`}
-            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+            className="text-center p-3 uppercase rounded-lg hover:opacity-95"
           >
             Send message
           </Link>
