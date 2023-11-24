3d45ab68789160de441804419db01d1b73fce01e
Dmitrii <dimterion59@gmail.com>
2023-11-24 10:33:49 +0100

    adjust header for pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,14 +1,23 @@
+import Header from "../components/Header";
+
 export default function About() {
   return (
-    <div className="py-20 px-4 max-w-6xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 text-slate-800">
-        About MERNstack
-      </h1>
-      <p className="mb-4 text-slate-700">A full-stack application.</p>
-      <p className="mb-4 text-slate-700">
-        Built to practice MERN-stack techniques.
-      </p>
-      <p className="mb-4 text-slate-700">MongoDB, Express, React, Node.js.</p>
-    </div>
+    <>
+      <Header />
+      <section className="signIn-section">
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+          <h1 className="text-3xl font-bold mb-4 text-slate-800">
+            About MERNstack
+          </h1>
+          <p className="mb-4 text-slate-700">A full-stack application.</p>
+          <p className="mb-4 text-slate-700">
+            Built to practice MERN-stack techniques.
+          </p>
+          <p className="mb-4 text-slate-700">
+            MongoDB, Express, React, Node.js.
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
