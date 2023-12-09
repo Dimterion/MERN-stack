ed16576ed2b182ccd2ad54381a7398f551335dc8
Dmitrii <dimterion59@gmail.com>
2023-12-09 22:55:47 +0100

    add css img to objectives comp

--- client/src/components/Objectives.jsx
+++ client/src/components/Objectives.jsx
@@ -10,26 +10,35 @@ export default function Objectives() {
         <h2 className="uppercase my-1 border-b pb-4">
           Dev Ops Products System
         </h2>
-        <fieldset className="objectives-fieldset border-2 mt-8 p-4">
+        <fieldset className="objectives-fieldset border-2 mt-8 p-4 flex flex-row flex-wrap justify-between items-center">
           <legend className="uppercase px-2">Total Rating</legend>
-          <h2 className="text-4xl mb-4">A+</h2>
-          <ul className="objectives-ul text-lg ml-4">
-            <li>Serve the clean code</li>
-            <li>Protect the main branch</li>
-            <li>Uphold the documentation</li>
-            {classified ? (
-              <button
-                className="text-lg py-2 uppercase hover:opacity-80"
-                onClick={() => setClassified(false)}
-              >
-                [ Classified ]
-              </button>
-            ) : (
-              <li>
-                Any attempt to apply for a senior dev position results in denial
-              </li>
-            )}
-          </ul>
+          <div>
+            <h2 className="text-4xl mb-4">A+</h2>
+            <ul className="objectives-ul text-lg ml-4">
+              <li>Serve the clean code</li>
+              <li>Protect the main branch</li>
+              <li>Uphold the documentation</li>
+              {classified ? (
+                <button
+                  className="text-lg py-2 uppercase hover:opacity-80"
+                  onClick={() => setClassified(false)}
+                >
+                  [ Classified ]
+                </button>
+              ) : (
+                <li>
+                  Any attempt to apply for a senior dev position results in
+                  denial
+                </li>
+              )}
+            </ul>
+          </div>
+          <div className="objectives-img">
+            <div className="objectives-imgHelmet"></div>
+            <div className="objectives-imgVisor"></div>
+            <div className="objectives-imgMouth"></div>
+            <div className="objectives-imgChin"></div>
+          </div>
         </fieldset>
         <fieldset className="objectives-fieldset border-2 mt-8 p-4">
           <legend className="uppercase px-2">Total XP Earned</legend>
