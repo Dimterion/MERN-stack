04dbfedd6b44aea744d2db4f0c0b181072b4d728
Dmitrii <dimterion59@gmail.com>
2023-12-30 09:52:57 +0100

    adjust profile comp classes & style

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,12 +2,10 @@ import profileImg from "../assets/images/profileImg.jpg";
 
 export default function Profile() {
   return (
-    <article className="profile-article text-center border-2 p-2">
+    <article className="profile-article">
       <div className="border-2">
-        <h2 className="profile-articleHeadingOne text-4xl my-2">
-          I am very developy.
-        </h2>
-        <h2 className="profile-articleHeadingTwo text-2xl my-2">
+        <h2 className="profile-articleHeadingOne">I am very developy.</h2>
+        <h2 className="profile-articleHeadingTwo">
           Will be a <span className="profile-articleSpan">cute</span> good
           addition to your team.
         </h2>
@@ -16,15 +14,13 @@ export default function Profile() {
           src={profileImg}
           alt="Cute cat in futuristic suit."
         />
-        <h2 className="profile-articleHeadingThree text-xl my-2">
-          Pretty please.
-        </h2>
-        <p className="profile-articleParagraph m-2 text-left px-10">
+        <h2 className="profile-articleHeadingThree">Pretty please.</h2>
+        <p className="profile-articleParagraph">
           P.S. Will work for at least 5 years of minimum, required, necessary,
           mandatory, obligatory experience to get a position of an entry-level
           associate junior assistant (to the) seasoned developer.
         </p>
-        <div className="profile-articleDiv text-left mb-2 px-12">
+        <div className="profile-articleDiv">
           <p>Yours truly,</p>
           <p>Robocat</p>
         </div>
